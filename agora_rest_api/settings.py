2c21934b15384d0f0c0eedd5b0bfc163b61fd5d7
freestyle076 <kylehandy3220@gmail.com>
2014-10-24 11:55:23 -0700

    Changed database settings to our database parameters

--- agora_rest_api/settings.py
+++ agora_rest_api/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'agora_rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sustainability_app',
+        'USER': 'khandy',
+        'PASSWORD': 'Rusty3220',
+        'HOST': '147.222.165.3',
+        'PORT': '3306'  
     }
 }
 
