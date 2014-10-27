7a8b4386f3a6eeef5cee3b1f103e03306a803e48
freestyle076 <kylehandy3220@gmail.com>
2014-10-27 13:37:37 -0700

    register router urls

--- agora_rest_api/urls.py
+++ agora_rest_api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'agora_rest_api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', include(router.urls)),
 )
