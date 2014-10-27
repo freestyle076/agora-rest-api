f97712588305b236ec8bb322ee9067e9802ecb26
freestyle076 <kylehandy3220@gmail.com>
2014-10-27 13:48:01 -0700

    my bad, underscores...

--- agora_rest_api/user_service/serializers.py
+++ agora_rest_api/user_service/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'firstname', 'lastname', 'phone')
+        fields = ('username', 'email', 'first_name', 'last_name', 'phone')
