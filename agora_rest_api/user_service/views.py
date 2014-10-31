a62b9564ff7da23c9aad951b0464f1e76f9e680c
freestyle076 <kylehandy3220@gmail.com>
2014-10-31 11:37:53 -0700

    try/except branch for trying both allowed email domains

--- agora_rest_api/user_service/views.py
+++ agora_rest_api/user_service/views.py
@@ -31,15 +31,24 @@ def ldap_authenticate(request):
         #attempt connection to ldap server
         handle = ldap.open('dc-ad-gonzaga.gonzaga.edu')
         try:
-            #attempt bind with username and password
-            handle.simple_bind_s('khandy@zagmail.gonzaga.edu', 's;jhf;ajsdhf;afh;s')
+            #attempt bind with username and password gonzaga.edu
+            handle.simple_bind_s('khandy@gonzaga.edu', 'wrong')
             #if successful return OK, username+pwd is in the ldap database!
             return HttpResponse(status=status.HTTP_200_OK)
+            
         except ldap.LDAPError, error_message:
-            #username+password not in ldap database, return bad request
-            response = HttpResponse(status=status.HTTP_400_BAD_REQUEST)
-            response.write(error_message)
-            return response
+            try:
+                #attempt bind with username and password gonzaga.edu
+                handle.simple_bind_S('khandy@zagmail.gonzaga.edu','wrong')
+                #if successful return OK, username+pwd is in the ldap database!
+                return HttpResponse(status=status.HTTP_200_OK)
+                
+            except ldap.LDAPError, error_message:
+                #username+password not in ldap database, return bad request
+                response = HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+                response.write(error_message)
+                return response
+                
     except ldap.LDAPError, error_message:
         #failure to connect to ldap server
         response = HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
