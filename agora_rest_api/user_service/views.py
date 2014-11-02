8c0a73cec2d286800cce72cda74081d139c6a862
freestyle076 <kylehandy3220@gmail.com>
2014-11-02 12:09:38 -0800

    sycning to latest changes

--- agora_rest_api/user_service/views.py
+++ agora_rest_api/user_service/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from django.http import HttpResponse
 import ldap
+import json
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -30,10 +31,9 @@ class LdapAuthRequestSerializer(serializers.HyperlinkedModelSerializer):
         model = LdapAuthRequest
         fields = ('username','password')
 
-@api_view(['GET'])
+@api_view(['POST'])
 def ldap_authenticate(request):
-    print request['username']
-    print request['password']
+    json_data = {}
     try:
         #attempt connection to ldap server
         handle = ldap.open('dc-ad-gonzaga.gonzaga.edu')
@@ -41,24 +41,27 @@ def ldap_authenticate(request):
             #attempt bind with username and password gonzaga.edu
             handle.simple_bind_s('khandy@gonzaga.edu', 'wrong')
             #if successful return OK, username+pwd is in the ldap database!
-            return HttpResponse(status=status.HTTP_200_OK)
+            json_data['email'] = 'khandy@gonzaga.edu'
+            response = HttpResponse(json.dumps(json_data),status=status.HTTP_200_OK,content_type='application/json')
+            return response
             
         except ldap.LDAPError, error_message:
             try:
                 #attempt bind with username and password gonzaga.edu
-                handle.simple_bind_S('khandy@zagmail.gonzaga.edu','wrong')
+                handle.simple_bind_s('khandy@zagmail.gonzaga.edu','wrong')
                 #if successful return OK, username+pwd is in the ldap database!
-                return HttpResponse(status=status.HTTP_200_OK)
+                json_data['email'] = 'khandy@gonzaga.edu'
+                response = HttpResponse(json.dumps(json_data),status=status.HTTP_200_OK,content_type='application/json')
+                return response
                 
             except ldap.LDAPError, error_message:
                 #username+password not in ldap database, return bad request
-                response = HttpResponse(status=status.HTTP_400_BAD_REQUEST)
-                response.write(error_message)
+                response = HttpResponse(status=status.HTTP_400_BAD_REQUEST,content_type='application/json')
                 return response
                 
     except ldap.LDAPError, error_message:
         #failure to connect to ldap server
-        response = HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        response = HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR,content_type='application/json')
         response.write(error_message)
         return response
         
