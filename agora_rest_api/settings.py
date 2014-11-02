4392da08a290da5b7e4dc74b7a16caec007adf0c
freestyle076 <kylehandy3220@gmail.com>
2014-11-02 12:09:06 -0800

    syncing with latest changes

--- agora_rest_api/settings.py
+++ agora_rest_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '_yx2*)2l9=s-!!kke!xj_p18td#ie^uci@^s0lv&!sor=)hr6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = False
 
--- agora_rest_api/urls.py
+++ agora_rest_api/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
-#from user_service import views
+from user_service import views
 
 router = routers.DefaultRouter()
-<<<<<<< HEAD
 router.register(r'users', views.UserViewSet)
-router.register(r'ldap', views.ldapViewSet)
-=======
 #router.register(r'users', views.UserViewSet)
->>>>>>> 2824223e4db6ffe319926745aa09fcf78a393187
 
 urlpatterns = patterns('',
     # Examples:
