64be5dbf231988253136c042ade9d4ce20362109
freestyle076 <kylehandy3220@gmail.com>
2014-11-04 11:57:58 -0800

    empty main file for LdapAuth test file directory

---
+++ test_files/LdapAuth/LdapAuth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LdapAuth
+//
+//  Created by Kyle Handy on 11/4/14.
+//  Copyright (c) 2014 Kyle Handy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
