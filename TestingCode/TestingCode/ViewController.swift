682610bf6b52e3cf517fceb674722e6c5773e743
TrentonMiller <trentonmiller@trentons-mini.gonzaga.edu>
2014-11-04 11:03:48 -0800

    created empty project in swift

---
+++ TestingCode/TestingCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestingCode
+//
+//  Created by Trenton Miller on 11/4/14.
+//  Copyright (c) 2014 Trenton Miller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
